d81b8a9c588567b821b51fcffc8e97de9eed4071
gellirosario <gellirosario@gmail.com>
2024-05-25 20:10:53 +0800

    Update ProfilePage

--- frontend-service/src/components/ProfilePage.js
+++ frontend-service/src/components/ProfilePage.js
@@ -46,39 +46,43 @@ function ProfilePage() {
   };
 
   return (
-    <div className="profile-container bg-gray-100 min-h-screen">
+    <div className="profile-container bg-blue-500 min-h-screen max-h-fit min-h-screen flex flex-col">
       <TopNav />
-      <div className="profile-picture-container flex justify-center items-center mt-8">
-        {/* Display uploaded profile picture */}
-        {profilePicture ? (
-          <img src={profilePicture} alt="Profile" className="profile-picture rounded-full" />
-        ) : (
-          <div className="default-profile-picture bg-gray-300 rounded-full flex justify-center items-center" onClick={handleUploadClick}>
-            Upload Profile Picture
+      <div className='flex items-center justify-center items-center'>
+        <div className='items-left bg-white shadow-md rounded-lg p-8 my-9 w-2/3'>
+          <div className="profile-picture-container flex justify-center items-center mt-8">
+            {/* Display uploaded profile picture */}
+            {profilePicture ? (
+              <img src={profilePicture} alt="Profile" className="profile-picture rounded-full" />
+            ) : (
+              <div className="default-profile-picture bg-gray-300 rounded-full flex justify-center items-center" onClick={handleUploadClick}>
+                Upload Profile Picture
+              </div>
+            )}
+            {/* Upload Profile Picture Button */}
+            <input type="file" onChange={handleProfilePictureUpload} accept="image/*" className="hidden" ref={fileInputRef} />
           </div>
-        )}
-        {/* Upload Profile Picture Button */}
-        <input type="file" onChange={handleProfilePictureUpload} accept="image/*" className="hidden" ref={fileInputRef} />
-      </div>
-      {/* Greeting */}
-      <div className="greeting-container text-center mt-4">
-        <h2 className="text-xl font-semibold">Hello User99999</h2>
-        <p>Been a member since {memberSinceDate}</p>
-      </div>
-      {/* Tabs */}
-      <div className="tabs-container flex justify-center mt-4">
-        <button className="tab bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-l-md">Leaderboards</button>
-        <button className="tab bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-r-md">View Events</button>
-        {/* Button for organizers to automatch volunteers */}
-        {/* Conditionally render based on user type (organizer or user) */}
-        {isOrganizer && (
-          <div className="flex items-center">
-            <input type="number" value={requiredVolunteers} onChange={(e) => setRequiredVolunteers(parseInt(e.target.value))} className="input-field mr-2" />
-            <button className="automatch-button bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 ml-2 rounded-md" onClick={handleAutomatchVolunteers}>
-              Automatch Volunteers
-            </button>
+          {/* Greeting */}
+          <div className="greeting-container text-center mt-4">
+            <h2 className="text-xl font-semibold">Hello User99999</h2>
+            <p>Been a member since {memberSinceDate}</p>
           </div>
-        )}
+          {/* Tabs */}
+          <div className="tabs-container flex justify-center mt-4">
+            <button className="tab bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-l-md">Leaderboards</button>
+            <button className="tab bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-r-md">View Events</button>
+            {/* Button for organizers to automatch volunteers */}
+            {/* Conditionally render based on user type (organizer or user) */}
+            {isOrganizer && (
+              <div className="flex items-center">
+                <input type="number" value={requiredVolunteers} onChange={(e) => setRequiredVolunteers(parseInt(e.target.value))} className="input-field mr-2" />
+                <button className="automatch-button bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 ml-2 rounded-md" onClick={handleAutomatchVolunteers}>
+                  Automatch Volunteers
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
