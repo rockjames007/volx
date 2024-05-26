ce2238f21335786ad4507a52a244d199f371ed58
gellirosario <gellirosario@gmail.com>
2024-05-26 10:25:38 +0800

    Update EventPage

--- frontend-service/src/components/EventPage.js
+++ frontend-service/src/components/EventPage.js
@@ -1,15 +1,17 @@
 import { useRecoilState } from 'recoil';
-import { eventCategories } from '../data/event-mainpage';
+import { eventCategories} from '../data/event-mainpage';
 import { eventIdState } from '../state/recoilstate';
+import '../App.css';
 
 const EventPage = () => {
-    const [eventId, setEventId] = useRecoilState(eventIdState);
-    const event = eventCategories.find(x => x.eventId === eventId);
+    
+    const [eventId] = useRecoilState(eventIdState);
+  const event = eventCategories.find(x => x.eventId === eventId);
     return <>
         {event &&
             <div>
-                <div className="flex flex-col items-center justify-center bg-white bg-opacity-25 rounded-xl shadow-xl p-4 backdrop-filter backdrop-blur-lg mt-10 mx-auto w-1/2">
-                    <img src={event.imageUrl} alt="Image" className="h-80  object-contain" />
+                <div className="bg-blue-500 min-h-screen flex flex-col"> <div className='flex items-center justify-center'><div className="items-left bg-white shadow-md rounded-lg p-8 max-w-md my-9 max-w-max">
+                    <img src={event.imageUrl} alt="Image" className="h-80 object-contain" />
                     <div className='bg-white h-30 bg-opacity-25 rounded-xl shadow-xl p-4 backdrop-filter backdrop-blur-lg'>
                         <div className="text-center text-black font-bold overflow-hidden overflow-ellipsis whitespace-nowrap">{event.eventName}</div>
                         <div className="text-center text-black">{event.date}</div>
@@ -18,7 +20,7 @@ const EventPage = () => {
                     </div>
                     <div>
                         <button type="submit" className="w-full bg-green-600 text-white font-bold py-2 px-4 rounded-md focus:bg-green-700 hover:bg-green-700">Apply</button>
-                    </div>
+                    </div></div></div>
                 </div>
             </div>}
     </>;
