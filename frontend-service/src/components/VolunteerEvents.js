c08b2d0834e8b777681d2ed79812a51730c0c313
gellirosario <gellirosario@gmail.com>
2024-05-26 10:08:13 +0800

    Update VolunteerEvents

--- frontend-service/src/components/VolunteerEvents.js
+++ frontend-service/src/components/VolunteerEvents.js
@@ -10,25 +10,25 @@ function VolunteerEvents() {
       <div className="border-b border-gray-200">
         <nav className="-mb-px flex space-x-8">
           <button
-            className={`border-transparent text-white  hover:border-gray-300 ${activeTab === 'personalised' ? 'border-b-2  text-white bg-green-600' : ''} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm rounded-lg`}
+            className={`border-transparent text-white py-2 px-4 rounded-md hover:bg-green-700  ${activeTab === 'personalised' ? 'border-b-2  text-white bg-green-600' : ''}  whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm rounded-lg`}
             onClick={() => setActiveTab('personalised')}
           >
             Personalised
           </button>
           <button
-            className={`border-transparent text-white  hover:border-gray-300 ${activeTab === 'volunteeringrequest' ? 'border-b-2  text-white bg-green-600' : ''} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm rounded-lg`}
+            className={`border-transparent text-white py-2 px-4 rounded-md hover:bg-green-700 ${activeTab === 'volunteeringrequest' ? 'border-b-2  text-white bg-green-600' : ''} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm rounded-lg`}
             onClick={() => setActiveTab('volunteeringrequest')}
           >
             Volunteering Request
           </button>
           <button
-            className={`border-transparent text-white  hover:border-gray-300 ${activeTab === 'upcoming' ? 'border-b-2  text-white bg-green-600' : ''} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm rounded-lg`}
+            className={`border-transparent text-white py-2 px-4 rounded-md hover:bg-green-700 ${activeTab === 'upcoming' ? 'border-b-2  text-white bg-green-600' : ''} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm rounded-lg`}
             onClick={() => setActiveTab('upcoming')}
           >
             Upcoming
           </button>
           <button
-            className={`border-transparent text-white  hover:border-gray-300 ${activeTab === 'attended' ? 'border-b-2  text-white bg-green-600' : ''} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm rounded-lg`}
+            className={`border-transparent text-white py-2 px-4 rounded-md hover:bg-green-700 ${activeTab === 'attended' ? 'border-b-2  text-white bg-green-600' : ''} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm rounded-lg`}
             onClick={() => setActiveTab('attended')}
           >
             Attended
@@ -36,7 +36,7 @@ function VolunteerEvents() {
           
         </nav>
       </div>
-      <div className="mt-4">
+      <div className="bg-transparent p-3 mt-3 mx-auto event-main-card">
         {activeTab === 'personalised' && <PersonalisedPage/>}
         {activeTab === 'upcoming' && <RequestPage/>}
         {activeTab === 'attended' && <div>Attended content...</div>}
